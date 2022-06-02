428848e6ef12614e45d376e8cf2c64503a06b29a
pzareba22 <pzareba22@gmail.com>
2022-06-02 03:31:59 +0200

    implemented ElevatorController

---
+++ src/logic/ElevatorController.ts
@@ -0,0 +1,35 @@
+import Elevator from "./Elevator";
+
+class ElevatorController {
+    readonly elevatorsNumber: number;
+    // Pivate or ES6 private?
+    private elevators: Array<Elevator>;
+    constructor(elevatorsNumber: number) {
+        this.elevatorsNumber = elevatorsNumber;
+        this.elevators = new Array(elevatorsNumber);
+        for (let i = 0; i < elevatorsNumber; i++) {
+            this.elevators[i] = new Elevator();
+        }
+        console.log("Done initializing");
+    }
+
+    addElevatorStop(
+        elevatorNumber: number,
+        floorFrom: number,
+        floorTo: number
+    ) {
+        this.elevators[elevatorNumber].addStop(floorFrom, floorTo);
+    }
+
+    getElevatorPositoins() {
+        return this.elevators.map((elevator) => elevator.getFloor());
+    }
+
+    update() {
+        for (let i = 0; i < this.elevators.length; i++) {
+            this.elevators[i].move();
+        }
+    }
+}
+
+export default ElevatorController;
