57280de0e3aa0d2c71761124186fadd92fcb04f7
pzareba22 <pzareba22@gmail.com>
2022-06-05 01:53:48 +0200

    added a new test

--- src/tests/Elevator.test.ts
+++ src/tests/Elevator.test.ts
@@ -1,11 +1,19 @@
-import Elevator from "../logic/Elevator";
 import ElevatorController from "../logic/ElevatorController";
 
 test("Go up one floor", () => {
     const elevatorController = new ElevatorController(1);
     elevatorController.addElevatorStop(0, 0, 1);
     elevatorController.update();
-    const res = elevatorController.getElevatorPositoins();
+    const res = elevatorController.getElevatorPositions();
     // console.log(`res is ${res}`);
     expect(res).toEqual([1]);
+});
+
+test("Go to the correct floor first", () => {
+    const elevatorController = new ElevatorController(1);
+    elevatorController.addElevatorStop(0, 2, 1);
+    elevatorController.update();
+    const res = elevatorController.getElevatorPositions();
+    // console.log(`res is ${res}`);
+    expect(res).toEqual([2]);
 });
