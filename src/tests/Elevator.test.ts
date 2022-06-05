a600000974bd157da992f34d78ed53647bea3b55
Paweł Zaręba <pzareba22@gmail.com>
2022-06-05 16:12:11 +0200

    changed the tests to test one instance of Elevator

--- src/tests/Elevator.test.ts
+++ src/tests/Elevator.test.ts
@@ -3,21 +3,19 @@ import Elevator from "../logic/Elevator";
 import ElevatorController from "../logic/ElevatorController";
 
 test("Go up one floor", () => {
-    const elevatorController = new ElevatorController(1);
-    elevatorController.addElevatorStop(0, 0, 1);
-    elevatorController.update();
-    const res = elevatorController.getElevatorPositions();
-    // console.log(`res is ${res}`);
-    expect(res).toEqual([1]);
+    const elevator = new Elevator();
+    elevator.addStop(0, 1);
+    elevator.move();
+    const res = elevator.getFloor();
+    expect(res).toEqual(1);
 });
 
 test("Go to the correct floor first", () => {
-    const elevatorController = new ElevatorController(1);
-    elevatorController.addElevatorStop(0, 2, 1);
-    elevatorController.update();
-    const res = elevatorController.getElevatorPositions();
-    // console.log(`res is ${res}`);
-    expect(res).toEqual([2]);
+    const elevator = new Elevator();
+    elevator.addStop(2, 1);
+    elevator.move();
+    const res = elevator.getFloor();
+    expect(res).toEqual(2);
 });
 
 test("Multiple floor requests", () => {
