716e276f8ae4809d9885033b52bbd81bf8c5a6fb
Paweł Zaręba <pzareba22@gmail.com>
2022-06-06 00:49:06 +0200

    extracted the padding to a variable

--- src/components/Elevator.tsx
+++ src/components/Elevator.tsx
@@ -5,11 +5,13 @@ type Props = {
     floor?: number;
 };
 
+const elevatorPadding = 0.2;
+
 const Elevator: React.FC<Props> = ({ floor }) => {
-    const shaftPos = floor ? floor * 5 + 0.2 : 0.2;
+    const shaftPos = floor ? floor * 5 + elevatorPadding : elevatorPadding;
 
     return (
-        <div className="elevator">
+        <div className="elevator" style={{ padding: `${elevatorPadding}em` }}>
             <div
                 className="shaft"
                 style={{ bottom: shaftPos.toString() + "em" }}
