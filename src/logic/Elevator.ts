24269e417f4c8b8efd866ec84e7aafbf6e679b8e
Paweł Zaręba <pzareba22@gmail.com>
2022-06-06 00:49:40 +0200

    rewrote the logic of addStop and move

--- src/logic/Elevator.ts
+++ src/logic/Elevator.ts
@@ -1,12 +1,13 @@
-import { DirectionType, findIndex, FloorEntry, insertSorted } from "./utils";
+import { findIndex, insertSorted } from "./utils";
+import { DirectionType, FloorEntry } from "./types";
 
 class Elevator {
     private floor: number;
-    readonly stops: Array<FloorEntry>;
+    stops: Array<FloorEntry>;
     private direction: DirectionType;
     constructor() {
         this.floor = 0;
-        this.stops = [];
+        this.stops = [{ floor_no: 0, floors_to: [] }];
         this.direction = "UP";
     }
 
@@ -18,68 +19,69 @@ class Elevator {
         return this.floor;
     }
 
-    // Zrobic overload
-    addStop(floorFrom: number, floorTo: number) {
+    addStop(floorFrom: number, floorTo?: number) {
         if (floorFrom === floorTo) return;
-        let newEntry: FloorEntry;
-        if (floorFrom === this.floor) {
-            newEntry = {
-                floor_no: floorTo,
-                floors_to: [],
-            };
-        } else {
-            newEntry = {
-                floor_no: floorFrom,
-                floors_to: floorTo != -1 ? [floorTo] : [],
-            };
-        }
-        if (this.stops.length == 0) {
-            this.stops.push(newEntry);
+        let newEntry: FloorEntry = {
+            floor_no: floorFrom,
+            floors_to: floorTo !== undefined ? [floorTo] : [],
+        };
+
+        const index = findIndex(this.stops, floorFrom, (x) => x.floor_no);
+        if (
+            index < this.stops.length &&
+            this.stops[index].floor_no == floorFrom &&
+            floorTo !== undefined
+        ) {
+            insertSorted(this.stops[index].floors_to, floorTo, (x) => x);
             return;
         }
-        const fromIndex = findIndex(this.stops, floorFrom);
-        if (
-            fromIndex < this.stops.length &&
-            this.stops[fromIndex].floor_no == floorFrom
-        ) {
-            insertSorted(this.stops[fromIndex].floors_to, floorTo);
-        } else {
-            this.stops.splice(fromIndex, 0, newEntry);
-        }
+        this.stops.splice(index, 0, newEntry);
     }
 
     move() {
         if (this.stops.length === 0) {
             return;
         }
-        const currentPositionIndex = findIndex(this.stops, this.floor);
+        let currentPositionIndex = findIndex(
+            this.stops,
+            this.floor,
+            (x) => x.floor_no
+        );
+
+        if (
+            this.stops[currentPositionIndex].floors_to.length === 0 &&
+            this.stops.length === 1
+        )
+            return;
+
         let newPosition;
-        if (this.direction === "DOWN") {
-            if (
-                currentPositionIndex === 0 &&
-                this.stops[0].floor_no > this.floor
-            ) {
+
+        this.stops[currentPositionIndex].floors_to.forEach((floor) =>
+            this.addStop(floor)
+        );
+        currentPositionIndex = findIndex(
+            this.stops,
+            this.floor,
+            (x) => x.floor_no
+        );
+
+        if (this.direction == "DOWN") {
+            if (currentPositionIndex === 0) {
                 this.direction = "UP";
-                newPosition = this.stops[currentPositionIndex];
-                this.stops.splice(currentPositionIndex, 1);
+                newPosition = this.stops[currentPositionIndex + 1];
             } else {
                 newPosition = this.stops[currentPositionIndex - 1];
-                this.stops.splice(currentPositionIndex - 1, 1);
             }
         } else {
-            if (currentPositionIndex == this.stops.length) {
+            if (currentPositionIndex == this.stops.length - 1) {
                 this.direction = "DOWN";
-                newPosition = this.stops[this.stops.length - 1];
-                this.stops.splice(this.stops.length - 1, 1);
+                newPosition = this.stops[currentPositionIndex - 1];
             } else {
-                newPosition = this.stops[currentPositionIndex];
-                this.stops.splice(currentPositionIndex, 1);
+                newPosition = this.stops[currentPositionIndex + 1];
             }
         }
         this.floor = newPosition.floor_no;
-        for (let i = 0; i < newPosition.floors_to.length; i++) {
-            this.addStop(newPosition.floors_to[i], -1);
-        }
+        this.stops.splice(currentPositionIndex, 1);
     }
 }
 
