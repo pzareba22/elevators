adccff6066eba537a336828be304208ca06974fa
pzareba22 <pzareba22@gmail.com>
2022-06-06 19:06:21 +0200

    new stylesheet

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { FormData } from "./types";
+import "../styles/Controls.sass";
 
 type Props = {
     elevatorNum: number;
