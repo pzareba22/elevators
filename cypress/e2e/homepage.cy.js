99bad02ee76355d9587b94394d0a0a6462d33f1f
pzareba22 <pzareba22@gmail.com>
2022-06-06 19:06:02 +0200

    more tests

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,9 +1,17 @@
+const elevatorPadding = 0.2;
+const calculateBottom = (floor, maxFloor) => {
+    const floorHeight = (30 + 2 * elevatorPadding) / (maxFloor + 1);
+    const res = (floor * floorHeight + elevatorPadding) * 16;
+    return parseInt(res);
+};
+
 describe("Check if the page renders correctly", () => {
     beforeEach(() => cy.visit("/"));
 
     it("Renders all components", () => {
         cy.get(".app").should("exist");
         cy.get(".elevatorsContainer").should("exist");
+        cy.get(".setupDialog").should("exist");
         cy.get(".elevatorControls").should("exist");
         cy.get(".submitButton").should("exist");
         cy.get(".requestsContainer").should("exist");
@@ -17,7 +25,7 @@ describe("Check if the page renders correctly", () => {
     });
 });
 
-describe("Check if the elevators move correctly", () => {
+describe("Checks if requests appear correctly", () => {
     beforeEach(() => {
         cy.visit("/");
         cy.get("#elevatorsNoInput").type("5");
@@ -26,7 +34,7 @@ describe("Check if the elevators move correctly", () => {
         cy.get(".elevatorContainer").should("have.length", 5);
     });
 
-    it("Checks if requests appear correctly", () => {
+    it("Creates one request", () => {
         cy.get("#elevatorNo").type("1");
         cy.get("#floorFrom").type("5");
         cy.get("#floorTo").type("3");
@@ -36,17 +44,53 @@ describe("Check if the elevators move correctly", () => {
         cy.get(".requestBox > p").eq(1).should("have.text", "From: 5");
         cy.get(".requestBox > p").eq(2).should("have.text", "To: 3");
     });
+});
+
+describe("Check if the elevators move correctly", () => {
+    beforeEach(() => {
+        cy.visit("/");
+        cy.get("#elevatorsNoInput").type("5");
+        cy.get("#floorNoInput").type("6");
+        cy.get(".setupDialog > form").submit();
+        cy.get(".elevatorContainer").should("have.length", 5);
+    });
 
     it("Moves one elevator correctly", () => {
         cy.get("#elevatorNo").type("0");
         cy.get("#floorFrom").type("2");
         cy.get("#floorTo").type("1");
         cy.get(".elevatorControls > form").submit();
+
         cy.get(".submitButton").click();
-        cy.get(".shaft").eq(0).should("have.text", "2");
-        // Check if padding is right
+        // cy.get(".shaft")
+        //     .eq(0)
+        //     .should("have.text", "2")
+        //     .should("have.css", "bottom", `${calculateBottom(2, 5)}px`);
+
         cy.get(".submitButton").click();
-        cy.get(".shaft").eq(0).should("have.text", "1");
-        // Check if padding is right
+        cy.get(".shaft")
+            .eq(0)
+            // .should("have.text", "1")
+            .should(($div) => {
+                expect($div).to.have.text("1");
+                const bottom = parseInt($div.css("bottom"));
+                expect(bottom).to.equal(calculateBottom(1, 5));
+            });
+    });
+
+    it("Moves multiple elevators correctly", () => {
+        cy.get("#elevatorNo").type("3");
+        cy.get("#floorFrom").type("4");
+        cy.get("#floorTo").type("2");
+        cy.get(".elevatorControls > form").submit();
+
+        cy.get("#elevatorNo").clear().type("2");
+        cy.get("#floorFrom").clear().type("1");
+        cy.get("#floorTo").clear().type("4");
+        cy.get(".elevatorControls > form").submit();
+
+        cy.get(".submitButton").click();
+        cy.get(".shaft").eq(3).should("have.text", "4");
+        cy.get(".shaft").eq(2).should("have.text", "1");
     });
 });
