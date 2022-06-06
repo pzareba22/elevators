be6e375eeb6b4f301071d76ce40b6b13a7c92eb1
Paweł Zaręba <pzareba22@gmail.com>
2022-06-06 02:19:34 +0200

    added SetupDialog component

---
+++ src/components/SetupDialog.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import "../styles/SetupDialog.sass";
+import { SetupData } from "./types";
+
+type Props = {
+    submitData: (data: SetupData) => void;
+};
+
+const SetupDialog: React.FC<Props> = ({ submitData }) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<SetupData>();
+    return (
+        <div className="setupDialog">
+            <form onSubmit={handleSubmit(submitData)}>
+                <label>Liczba wind:</label>
+                <input
+                    type="number"
+                    {...register("elevatorsNo", { required: true, min: 0 })}
+                />
+                <label>Liczba pieter w windzie</label>
+                <input
+                    type="number"
+                    {...register("floorNo", { required: true, min: 0 })}
+                />
+                <input type="submit" value="OK" />
+            </form>
+        </div>
+    );
+};
+
+export default SetupDialog;
--- src/components/types.ts
+++ src/components/types.ts
@@ -3,3 +3,8 @@ import { RequestType } from "../logic/types";
 export type ElevatorRequest = RequestType & { elevatorNo: number };
 
 export type FormData = ElevatorRequest;
+
+export type SetupData = {
+    elevatorsNo: number;
+    floorNo: number;
+};
