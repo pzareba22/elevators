985e6df42649df270b49f723a003c92cc8603aca
Paweł Zaręba <pzareba22@gmail.com>
2022-06-07 22:36:24 +0200

    annotations

--- src/logic/ElevatorController.ts
+++ src/logic/ElevatorController.ts
@@ -14,26 +14,41 @@ class ElevatorController {
         this.requests = Array.from({ length: elevatorsNumber }, () => []);
     }
 
-    addElevatorStop(
-        elevatorNumber: number,
-        floorFrom: number,
-        floorTo: number
-    ) {
-        this.elevators[elevatorNumber].addStop(floorFrom, floorTo);
-        this.requests[elevatorNumber].push({
+    /**
+     * Adds a stop to an elevators internal state
+     * @param elevatorID ID of the desired elevator
+     * @param floorFrom Number representing floor from which the elevator has to move
+     * @param floorTo Number representing floor to which the elevator has to move
+     */
+    addElevatorStop(elevatorID: number, floorFrom: number, floorTo: number) {
+        if (elevatorID < 0 || elevatorID >= this.elevatorsNumber) {
+            throw new Error("Invalid elevatorID: " + elevatorID.toString());
+        }
+        this.elevators[elevatorID].addStop(floorFrom, floorTo);
+        this.requests[elevatorID].push({
             floorFrom: floorFrom,
             floorTo: floorTo,
         });
     }
 
+    /**
+     * @returns An array of numbers representing positions of the elevators
+     */
     getElevatorPositions() {
         return this.elevators.map((elevator) => elevator.getFloor());
     }
 
+    /**
+     *
+     * @returns An array of all currently loaded requests
+     */
     getRequests() {
         return this.requests.flat();
     }
 
+    /**
+     * Updates position of each elevator once
+     */
     update() {
         for (let i = 0; i < this.elevators.length; i++) {
             this.elevators[i].move();
