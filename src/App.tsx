a9e08dba7e5498aaf41f3153ff0c592769a165ce
Paweł Zaręba <pzareba22@gmail.com>
2022-06-08 00:01:37 +0200

    got rid of unnecesary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useMemo, useState } from "react";
-import Elevator from "./components/Elevator";
 import ElevatorController from "./logic/ElevatorController";
 import "./styles/App.sass";
 import Controls from "./components/Controls";
